92040ae93310badc802b3255f1f49bdb4e1023b9
Sascha Selzer <sascha.selzer@gmail.com>
2024-06-26 17:26:02 +0200

    Fix broken eslint diagnostics

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,9 +6,12 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
+        require 'none-ls.diagnostics.eslint_d',
       },
     }
     vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = '[F]ormat' })
   end,
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim',
+  },
 }
