27d92d72c88f097fad3d79e9a3086f9fbc59b0c5
Sascha Selzer <sascha.selzer@gmail.com>
2024-11-13 11:48:02 +0100

    make home-manager file paths relative

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -12,16 +12,16 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".zshrc".source = ~/.dotfiles/zsh/.zshrc;
-    ".config/starship.toml".source = ~/.dotfiles/starship/starship.toml;
-    ".config/nvim".source = ~/.dotfiles/nvim;
-    ".config/zellij".source = ~/.dotfiles/zellij;
-    ".config/delta".source = ~/.dotfiles/delta;
-    ".config/lazygit".source = ~/.dotfiles/lazygit;
-    ".config/bat".source = ~/.dotfiles/bat;
-    ".config/kitty".source = ~/.dotfiles/kitty;
-    "fzf-git.sh".source = ~/.dotfiles/fzf-git.sh;
-    ".tmux.conf".source = ~/.dotfiles/tmux/.tmux.conf;
+    ".zshrc".source = ../zsh/.zshrc;
+    ".config/starship.toml".source = ../starship/starship.toml;
+    ".config/nvim".source = ../nvim;
+    ".config/zellij".source = ../zellij;
+    ".config/delta".source = ../delta;
+    ".config/lazygit".source = ../lazygit;
+    ".config/bat".source = ../bat;
+    ".config/kitty".source = ../kitty;
+    "fzf-git.sh".source = ../fzf-git.sh;
+    ".tmux.conf".source = ../tmux/.tmux.conf;
   };
 
   home.sessionVariables = {
