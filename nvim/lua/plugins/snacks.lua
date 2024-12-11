aba62142f8fcdb48c8527f562d55d8581e16db4c
sselzer <sascha.selzer@gmail.com>
2024-12-11 21:09:54 +0100

    Add snack

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    lazygit = {
+    }
+  },
+  keys = {
+    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit current file history" },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>ö", function() Snacks.terminal() end, desc = "Toggle Terminal" },
+  },
+}
