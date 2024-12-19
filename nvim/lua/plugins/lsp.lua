60721741b9b7e3221647d4c4e33bec2c9cbd7401
sselzer <sascha.selzer@gmail.com>
2024-12-19 07:37:25 +0100

    Update lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,39 +1,41 @@
 return {
   {
-    "williamboman/mason.nvim",
+    'williamboman/mason.nvim',
     opts = {},
   },
   {
-    "williamboman/mason-lspconfig",
+    'williamboman/mason-lspconfig',
     opts = {
       ensure_installed = {
-        "lua_ls",
-        "ts_ls",
-        "rust_analyzer",
+        'lua_ls',
+        'ts_ls',
+        'rust_analyzer',
       },
     },
   },
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require 'lspconfig'
+      lspconfig.lua_ls.setup {
         capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
+      }
+      lspconfig.ts_ls.setup {
         capabilities = capabilities,
-      })
-      lspconfig.rust_analyzer.setup({
+      }
+      lspconfig.rust_analyzer.setup {
         capabilities = capabilities,
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Go to [d]efinition" })
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code [A]ction" })
+      }
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>cd', vim.lsp.buf.definition, { desc = 'Go to [d]efinition' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code [A]ction' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>cr', vim.lsp.buf.rename, { desc = '[R]ename' })
+      vim.keymap.set({'n', 'v'}, '<leader>cs', vim.lsp.buf.document_symbol, {desc = '[S]ymbols'})
     end,
     dependencies = {
-      { "folke/neodev.nvim", opts = {} },
-      { "j-hui/fidget.nvim", opts = {} },
+      { 'folke/neodev.nvim', opts = {} },
+      { 'j-hui/fidget.nvim', opts = {} },
     },
   },
 }
