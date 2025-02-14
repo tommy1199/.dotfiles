81768250b47663e5d2988c7bd2db91bcb29787d8
sselzer <sascha.selzer@gmail.com>
2025-02-14 21:37:09 +0100

    Remove unneeded configuration

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -112,10 +112,6 @@
 
       security.pam.enableSudoTouchIdAuth = true;
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
