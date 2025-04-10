baf7edd509ab30fc62d4f5f64535e7c029f8abb5
sselzer <sascha.selzer@gmail.com>
2025-04-10 22:30:32 +0200

    add tuckr command to setup file

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@
 sh <(curl -L https://nixos.org/nix/install)
 nix-shell -p git --run 'git clone https://github.com/tommy1199/.dotfiles' ~/.dotfiles
 nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake ~/.dotfiles/nix-darwin#devmac
+tuckr add \*
 softwareupdate --install-rosetta --agree-to-license
