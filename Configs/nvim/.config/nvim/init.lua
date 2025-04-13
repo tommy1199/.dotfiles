e226d1f262666708086edf01e056a1c08a3ae246
sselzer <sascha.selzer@gmail.com>
2025-04-13 21:58:59 +0200

    update nvim configuration by using LazyVim

--- Configs/nvim/.config/nvim/init.lua
+++ Configs/nvim/.config/nvim/init.lua
@@ -1,22 +1,2 @@
-require('keymaps')
-
-vim.filetype.add({ extension = { templ = "templ" } })
-
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
-require('vim-options')
-require('lazy').setup("plugins")
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
