2de97cb7a2ae76af63e063e8903e050f162b55db
sselzer <sascha.selzer@gmail.com>
2026-09-17 11:23:41 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 ## Install dotfiles
+echo "Install rosetta"
+softwareupdate --install-rosetta --agree-to-license
+echo "Install homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Install nix"
 sh <(curl -L https://nixos.org/nix/install)
+echo "Clone dotfiles"
 nix-shell -p git --run 'git clone https://github.com/tommy1199/.dotfiles' ~/.dotfiles
+echo "Run nix switch"
 nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake ~/.dotfiles/nix-darwin#devmac
 tuckr add \*
-softwareupdate --install-rosetta --agree-to-license
